11b78b204e72286f70d7eb4680bff00744bb9b0f
Kriti Rai <kriti.rai@gmail.com>
2017-11-28 04:18:38 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+  def name=(name_of_the_dog)
+  @name = name_of_the_dog
+  def name
+    @name
+  end
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+  def breed
+    @breed
+  end
+end
